51412476bb00aa7f54cf5d46e2f7cba81fc9a88b
Samuel <samueltwumasi1290.com>
2024-01-30 15:05:22 +0000

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Samuel Twumasi on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
